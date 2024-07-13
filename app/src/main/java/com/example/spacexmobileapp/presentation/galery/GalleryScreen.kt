730c7afcea93e21dea9dc6da99e9d149d447c1cf
ForwardGT <ollmaincore@gmail.com>
2024-07-14 01:48:37 +0300

    fix GalleryScreen

--- app/src/main/java/com/example/spacexmobileapp/presentation/galery/GalleryScreen.kt
+++ app/src/main/java/com/example/spacexmobileapp/presentation/galery/GalleryScreen.kt
@@ -1,10 +1,10 @@
 package com.example.spacexmobileapp.presentation.galery
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,9 +16,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -26,16 +28,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.changedToUp
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
 fun GalleryScreen(
@@ -91,42 +89,34 @@ fun FullScreenPhoto(
     selectImage: String? = null,
     viewModel: GalleryScreenViewModel
 ) {
-    val scope = CoroutineScope(Dispatchers.Default)
-
+    var scale by remember { mutableFloatStateOf(1f) }
     var offsetX by remember { mutableFloatStateOf(0f) }
     var offsetY by remember { mutableFloatStateOf(0f) }
-    var zoom by remember { mutableFloatStateOf(1f) }
+    var size by remember { mutableStateOf(IntSize.Zero) }
+
+    val rememberTransformable = rememberTransformableState { zoom, pan, _->
+        scale *= zoom
+        offsetX += pan.x
+        offsetY += pan.y
+    }
+
+    LaunchedEffect(rememberTransformable.isTransformInProgress) {
+        if (!rememberTransformable.isTransformInProgress) {
+            scale = 1f
+            offsetX = 0f
+            offsetY = 0f
+            size = IntSize.Zero
+        }
+    }
 
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colorScheme.background.copy(alpha = 0.9f))
-            .pointerInput(Unit) {
-                detectTransformGestures(
-                    onGesture = { _, pan, gestureZoom, _ ->
-                        zoom *= gestureZoom
-                        offsetX += pan.x
-                        offsetY += pan.y
-                        scope.launch {
-                            awaitPointerEventScope {
-                                val event = awaitPointerEvent()
-                                val changes = event.changes
-                                changes.forEach { change ->
-                                    if (change.changedToUp()) {
-                                        zoom = 1f
-                                        offsetX = 0f
-                                        offsetY = 0f
-                                    }
-                                }
-                            }
-                        }
-                    }
-                )
-            }
+            .transformable(rememberTransformable)
             .clickable { viewModel.selectImage(null) }
     ) {
-        Log.d("TAG", "GalleryScreen: Zoom = $zoom")
         AsyncImage(
             model = selectImage,
             contentDescription = null,
@@ -137,8 +127,8 @@ fun FullScreenPhoto(
                 .graphicsLayer(
                     translationX = offsetX,
                     translationY = offsetY,
-                    scaleX = maxOf(1f, minOf(3f, zoom)),
-                    scaleY = maxOf(1f, minOf(3f, zoom))
+                    scaleX = maxOf(1f, minOf(3f, scale)),
+                    scaleY = maxOf(1f, minOf(3f, scale))
                 )
         )
         BackHandler {
